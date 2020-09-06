0c463c114287405770206140e4abc2ed0879eebb
Bismilah Langford <bismilah.lf@gmail.com>
2020-09-06 19:01:16 +0200

    Refactor submit handler

--- scripts.js
+++ scripts.js
@@ -1,18 +1,20 @@
-const baseEndpoint = "http://www.recipepuppy.com/api";
-const proxy = "https://cors-anywhere.herokuapp.com/";
+const baseEndpoint = `http://www.recipepuppy.com/api`;
+const proxy = `https://cors-anywhere.herokuapp.com/`;
 const form = document.querySelector(".search");
 
 async function fetchRecipes(query) {
     const res = await fetch(`${proxy}${baseEndpoint}?q=${query}`);
     const data = await res.json();
-    console.log(data)
+    return data;
 }
 
-//fetchRecipes("salad");
-
-function handleSubmit(event) {
+async function handleSubmit(event) {
     event.preventDefault();
-    console.log(event.currentTarget.query.value)
+    const element = event.currentTarget;
+    element.submit.disabled = true;
+    const recipes = await fetchRecipes(element.query.value);
+    console.log(recipes);
+    element.submit.disabled = false;
 }
 
 form.addEventListener("submit", handleSubmit);
